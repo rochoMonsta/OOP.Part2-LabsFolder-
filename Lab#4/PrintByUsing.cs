bccd7f2c6a4e49c47b1c7c26f6b43a7a68010d32
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-03-09 22:11:45 +0200

    git add

---
+++ Lab#4/PrintByUsing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab4
+{
+    class PrintByUsing<T> where T: IDocumentInfo<Person>, IInfo
+    {
+        public void PrintDocument(T printer)
+        {
+            Console.WriteLine("-----PRINTING DOCUMENT-----");
+            Console.WriteLine($"Document sending by\n\t{printer.SendingBy}");
+            Console.WriteLine($"Document sending to\n\t{printer.SendingTo}\n");
+            Console.WriteLine($"Text\n\t{printer.DocumentFilling}");
+            Console.WriteLine($"\nDocument printed by\n\t{printer}");
+        }
+    }
+}
---
+++ Lab#4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintByUsing<Printer<Person>> printByUsing = new PrintByUsing<Printer<Person>>();
+
+            var person1 = new Person("Tom", "Adams");
+            var person2 = new Person("Roman", "Cholkan");
+
+            Console.WriteLine("Enter your text:");
+            string text = Console.ReadLine();
+
+            Printer<Person> printer1 = new Printer<Person>()
+            {
+                Name = "DD2102",
+                Firm = "HP",
+                Type = "Laser print",
+                Price = 100,
+                SendingBy = person1,
+                SendingTo = person2,
+                DocumentFilling = text
+            };
+            Printer<Person> printer2 = new Printer<Person>()
+            {
+                Name = "KD2213",
+                Firm = "Epson",
+                Type = "Laser print",
+                Price = 350,
+                SendingBy = person1,
+                SendingTo = person2,
+                DocumentFilling = text
+            };
+            Console.Clear();
+            printByUsing.PrintDocument(printer1);
+            Swap<Printer<Person>>(ref printer1, ref printer2);
+            Console.WriteLine("\n");
+            printByUsing.PrintDocument(printer1);
+
+            Console.ReadLine();
+        }
+        public static void Swap <T> (ref T p1, ref T p2)
+        {
+            T fake = p1;
+            p1 = p2;
+            p2 = fake;
+        }
+    }
+}
