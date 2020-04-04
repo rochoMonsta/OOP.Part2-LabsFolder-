ce30caf8160cae3e1c340bb4f4308047ea1f710f
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-04-04 17:10:25 +0300

    git add

---
+++ v11/Lab#2/Lab2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter count of arc: ");
+            int inputData = Int32.Parse(Console.ReadLine());
+
+            List<Arc> inputArcs = new List<Arc>();
+
+            InputArc(inputArcs, inputData);
+            Console.Clear();
+            Console.WriteLine("Array without sort:");
+            PrintArray(inputArcs, inputData);
+
+            Sort sort = new Sort();
+            //sort.MergeSort(inputArcs, 0, inputData - 1);
+            sort.DoSelectionSort(inputArcs);
+            Console.WriteLine("Array after sort:");
+            PrintArray(inputArcs, inputData);
+
+            Arc arc = new Arc();
+            Console.WriteLine("Painted part: " + arc.GetDrawPart(inputArcs, inputData));
+
+            Console.ReadLine();
+        }
+        public static void InputArc(List<Arc> arcs, int count)
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                Console.WriteLine($"Enter x degree and y degree for {i + 1}:");
+                Console.Write($"\t{i + 1} X) ");
+                int x_degree = Int32.Parse(Console.ReadLine());
+
+                Console.Write($"\t{i + 1} Y) ");
+                int y_degree = Int32.Parse(Console.ReadLine());
+
+                arcs.Add(new Arc(x_degree, y_degree));
+            }
+        }
+        public static void PrintArray(List<Arc> arcs, int count)
+        {
+            for (int i = 0; i < count; ++i)
+                Console.WriteLine($"\t{i + 1}) " + arcs[i]);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ v11/Lab#2/Lab2/Sort.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Sort
+    {
+        //сортування злиттям
+        public void MergeSort(List<Arc> array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+        }
+        //сортування вибором
+        public void DoSelectionSort(List<Arc> array)
+        {
+            Arc minNumber;
+            for (int i = 0; i < array.Count - 1; i++)
+            {
+                minNumber = array[i];
+
+                for (int j = i + 1; j < array.Count; j++)
+                {
+                    if (minNumber.X_Degree == array[j].X_Degree)
+                    {
+                        minNumber = minNumber.Y_Degree < array[j].Y_Degree ? minNumber : array[j];
+                    }
+                    else
+                        minNumber = minNumber.X_Degree < array[j].X_Degree ? minNumber : array[j];
+                }
+                //int index = Array.IndexOf(array, minNumber);
+                int index = array.IndexOf(minNumber);
+                array[index] = array[i];
+                array[i] = minNumber;
+            }
+        }
+        private void Merge(List<Arc> array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new Arc[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left].X_Degree == array[right].X_Degree)
+                {
+                    if (array[left].Y_Degree < array[right].Y_Degree)
+                    {
+                        tempArray[index] = array[left];
+                        left++;
+                    }
+                    else
+                    {
+                        tempArray[index] = array[right];
+                        right++;
+                    }
+                }
+                else
+                {
+                    if (array[left].X_Degree < array[right].X_Degree)
+                    {
+                        tempArray[index] = array[left];
+                        left++;
+                    }
+                    else
+                    {
+                        tempArray[index] = array[right];
+                        right++;
+                    }
+                }
+                index++;
+            }
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
+    }
+}
