79d42ea44fd22a6b7070ea19c38d2803cb31d851
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-04-04 15:25:53 +0300

    git add

---
+++ v11/Lab#1/Lab1/Arc.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    class Arc
+    {
+        public int X_Degree { get; set; }
+        public int Y_Degree { get; set; }
+        public Arc() { }
+        public Arc(int X_Degree, int Y_Degree)
+        {
+            this.X_Degree = X_Degree;
+            this.Y_Degree = Y_Degree;
+        }
+        //метод який шукає замальовану частину.
+        public Arc GetDrawPart(Arc[] arcs, int count)
+        {
+            int X_MAX = 0, Y_MIN = 360;
+            for (int i = 0; i < count; ++i)
+            {
+                int BothMax = arcs[i].X_Degree > arcs[i].Y_Degree ? arcs[i].X_Degree : arcs[i].Y_Degree;
+                X_MAX = X_MAX > BothMax ? X_MAX : BothMax;
+
+                int BothMin = arcs[i].X_Degree < arcs[i].Y_Degree ? arcs[i].X_Degree : arcs[i].Y_Degree;
+                Y_MIN = Y_MIN < BothMin ? Y_MIN : BothMin;
+            }
+            return new Arc(X_MAX, Y_MIN);
+        }
+        public override string ToString()
+        {
+            return $"X: {X_Degree}; Y: {Y_Degree};";
+        }
+    }
+}
---
+++ v11/Lab#1/Lab1/Sort.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Sort
+    {
+        //сортування злиттям
+        public void MergeSort(Arc[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+        }
+        //сортування вибором
+        public void DoSelectionSort(Arc[] array)
+        {
+            Arc minNumber;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                minNumber = array[i];
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (minNumber.X_Degree == array[j].X_Degree)
+                    {
+                        minNumber = minNumber.Y_Degree < array[j].Y_Degree ? minNumber : array[j];
+                    }
+                    else
+                        minNumber = minNumber.X_Degree < array[j].X_Degree ? minNumber : array[j];
+                }
+                int index = Array.IndexOf(array, minNumber);
+                array[index] = array[i];
+                array[i] = minNumber;
+            }
+        }
+        private void Merge(Arc[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new Arc[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left].X_Degree == array[right].X_Degree)
+                {
+                    if (array[left].Y_Degree < array[right].Y_Degree)
+                    {
+                        tempArray[index] = array[left];
+                        left++;
+                    }
+                    else
+                    {
+                        tempArray[index] = array[right];
+                        right++;
+                    }
+                }
+                else
+                {
+                    if (array[left].X_Degree < array[right].X_Degree)
+                    {
+                        tempArray[index] = array[left];
+                        left++;
+                    }
+                    else
+                    {
+                        tempArray[index] = array[right];
+                        right++;
+                    }
+                }
+                index++;
+            }
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
+    }
+}
