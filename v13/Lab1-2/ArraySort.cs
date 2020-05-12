3da6288bc5512e8a3925fc58b8967597decc7e24
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-05-12 13:31:35 +0300

    git add

---
+++ v13/Lab1-2/ArraySort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class ArraySort
+    {
+        //Сортування вставкою
+        public void InsertionSort(List<int> stones)
+        {
+            int i, j;
+            for (i = 0; i < stones.Count; i++)
+            {
+                int fakeStone = stones[i];
+                for (j = i - 1; j >= 0 && stones[j] > fakeStone; j--)
+                    stones[j + 1] = stones[j];
+
+                stones[j + 1] = fakeStone;
+            }
+        }
+        //Швидке сорутвання
+        public void QuickSort(List<int> array, int minIndex, int maxIndex)
+        {
+            if (!(minIndex >= maxIndex))
+            {
+                var pivotIndex = Partition(array, minIndex, maxIndex);
+                QuickSort(array, minIndex, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, maxIndex);
+            }
+        }
+        private void Swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+        private int Partition(List<int> array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    int random_number = array[pivot];
+                    array[pivot] = array[i];
+                    array[i] = random_number;
+                }
+            }
+            pivot++;
+            int random_number1 = array[pivot];
+            array[pivot] = array[maxIndex];
+            array[maxIndex] = random_number1;
+            return pivot;
+        }
+    }
+}
---
+++ v13/Lab1-2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraySort array = new ArraySort();
+
+            Console.Write("Enter count of numbers in array: ");
+            int count = Int32.Parse(Console.ReadLine());
+
+            List<int> array1 = new List<int>();
+            List<int> array2 = new List<int>();
+
+            InputElementToArray(array1, count);
+            InputElementToArray(array2, count);
+
+            array.InsertionSort(array1);
+            array.QuickSort(array2, 0, array2.Count - 1);
+
+            PrintArray(array1);
+            PrintArray(array2);
+
+            Console.WriteLine("\nResult: " + GetSum(array1, array2));
+
+            Console.ReadLine();
+        }
+        public static int GetSum(List<int> arr1, List<int> arr2)
+        {
+            if (arr1.Count != arr2.Count)
+                return 0;
+            else
+            {
+                int sum = 0;
+                for (int i = 0; i < arr1.Count; ++i)
+                    sum += arr1[i] * arr2[arr1.Count - 1 - i];
+                return sum;
+            }
+        }
+        public static void InputElementToArray(List<int> array, int count)
+        {
+            Console.WriteLine("Enter elements to list:");
+            for (int i = 0; i < count; ++i)
+            {
+                Console.Write($"\t{i + 1}) ");
+                array.Add(Int32.Parse(Console.ReadLine()));
+            }
+        }
+        public static void PrintArray(List<int> array)
+        {
+            Console.WriteLine();
+            foreach (var element in array)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+    }
+}
