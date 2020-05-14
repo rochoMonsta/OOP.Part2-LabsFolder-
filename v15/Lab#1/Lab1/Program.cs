0802765538e494dd6606ef5712b2f2f7b92a07a3
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-05-14 19:10:26 +0300

    git add

---
+++ v15/Lab#1/Lab1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(DateTime.Now.Millisecond);
+            int[] segmentArray = new int[5000];
+            RandomGenerator(segmentArray);
+
+            Sorting sorting = new Sorting();
+            sorting.QuickSort(segmentArray, 0, segmentArray.Length - 1);
+
+            Console.WriteLine("Count = " + CalculateCountOfPossibleTriangles(segmentArray));
+            Console.ReadLine();
+        }
+        //Виведення елементів масиву цілих чисел
+        public static void PrintArray(int[] array)
+        {
+            Console.WriteLine();
+            foreach (var element in array)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+        //Метод для перевірки на існування трикутника (перевіряються суми довжин відрізків та умова для інсування трикутника)
+        //a + b> c, a + c> b, b + c> a, (a> 0, b> 0, c> 0)
+        public static int CalculateCountOfPossibleTriangles(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length - 2; ++i)
+                if (((arr[i] + arr[i + 1]) > arr[i + 2]) && ((arr[i + 1] + arr[i + 2]) > arr[i]) && ((arr[i] + arr[i + 2]) > arr[i + 1]))
+                    count++;
+            return count;
+        }
+        //Метод для генерації цілих чисел без повторів
+        public static void RandomGenerator(int[] array)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < array.Length; ++i)
+            {
+                int number = random.Next(1, 1000000);
+                while (array.Contains(number))
+                    number = random.Next(1, 1000000);
+                array[i] = number;
+            }
+        }
+    }
+}
---
+++ v15/Lab#1/Lab1/Sorting.cs
@@ -0,0 +1,48 @@
+﻿namespace Lab1
+{
+    class Sorting
+    {
+        //Сортування вставкою
+        public void InsertionSort(int[] array)
+        {
+            int i, j;
+            for (i = 0; i < array.Length; i++)
+            {
+                int fakeStone = array[i];
+                for (j = i - 1; j >= 0 && array[j] > fakeStone; j--)
+                    array[j + 1] = array[j];
+
+                array[j + 1] = fakeStone;
+            }
+        }
+        //Швидке сорутвання
+        public void QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (!(minIndex >= maxIndex))
+            {
+                var pivotIndex = Partition(array, minIndex, maxIndex);
+                QuickSort(array, minIndex, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, maxIndex);
+            }
+        }
+        private int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    int random_number = array[pivot];
+                    array[pivot] = array[i];
+                    array[i] = random_number;
+                }
+            }
+            pivot++;
+            int random_number1 = array[pivot];
+            array[pivot] = array[maxIndex];
+            array[maxIndex] = random_number1;
+            return pivot;
+        }
+    }
+}
