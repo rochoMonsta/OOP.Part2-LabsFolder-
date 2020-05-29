e2f201bfff00f6dfe490323d490e451dc36f0b65
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-05-29 23:13:35 +0300

    git add

---
+++ v9/Lab#1-2/Lab1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorting sorting = new Sorting();
+
+            Console.Write("Enter count of numbers: ");
+            int input_count = Int32.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+            NumberRandomGenerator(list, input_count);
+
+            Console.WriteLine("Array without sorting\n-------------------------------------------");
+            Print(list);
+            Console.WriteLine("Array after sorting\n---------------------------------------------");
+            sorting.MergeSort(list, 0, list.Count - 1);
+            Print(list);
+
+            Console.WriteLine($"Count of unique numbers = {GetCountOfUniqueNumbers(list)}");
+
+            Console.ReadLine();
+        }
+        public static void NumberRandomGenerator(List<int> array, int count)
+        {
+            Random random = new Random(Guid.NewGuid().ToByteArray().Sum(x => x));
+            for (int i = 0; i < count; ++i)
+                array.Add(random.Next(0, 1000));
+        }
+        public static int GetCountOfUniqueNumbers(List<int> array)
+        {
+            List<int> new_array = array.Distinct().ToList();
+            return new_array.Count;
+        }
+        public static void Print(List<int> array)
+        {
+            Console.WriteLine();
+            foreach (var element in array)
+                Console.Write(element + " ");
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ v9/Lab#1-2/Lab1/Sorting.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace Lab1
+{
+    class Sorting
+    {
+        //сортування злиттям
+        public void MergeSort(List<int> array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+        }
+        //Сортування вставкою
+        public void InsertionSort(List<int> stones)
+        {
+            int i, j;
+            for (i = 0; i < stones.Count; i++)
+            {
+                int fakeStone = stones[i];
+                for (j = i - 1; j >= 0 && stones[j] > fakeStone; j--)
+                    stones[j + 1] = stones[j];
+
+                stones[j + 1] = fakeStone;
+            }
+        }
+        private void Merge(List<int> array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left] == array[right])
+                {
+                    if (array[left] < array[right])
+                    {
+                        tempArray[index] = array[left];
+                        left++;
+                    }
+                    else
+                    {
+                        tempArray[index] = array[right];
+                        right++;
+                    }
+                }
+                else
+                {
+                    if (array[left] < array[right])
+                    {
+                        tempArray[index] = array[left];
+                        left++;
+                    }
+                    else
+                    {
+                        tempArray[index] = array[right];
+                        right++;
+                    }
+                }
+                index++;
+            }
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
+    }
+}
